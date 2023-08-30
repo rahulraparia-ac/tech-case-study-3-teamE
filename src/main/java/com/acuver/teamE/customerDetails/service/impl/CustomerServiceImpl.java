5b54fd56ef00ace423e7f85020c230db08fd535e
amritksacuver <amrit.sharma@acuverconsulting.com>
2023-08-30 09:54:18 +0530

    write back strategy

--- src/main/java/com/acuver/teamE/customerDetails/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/acuver/teamE/customerDetails/service/impl/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import com.acuver.teamE.customerDetails.entity.Customer;
 import com.acuver.teamE.customerDetails.entity.response.CustomerResponse;
 import com.acuver.teamE.customerDetails.repository.CustomerRepository;
 import com.acuver.teamE.customerDetails.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
@@ -13,22 +14,37 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 @CacheConfig(cacheNames = {"Customer"})
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
+    private static final String WRITE_BACK_CACHE_KEY = "customer:write_back";
+    private RedisTemplate<String, Customer> redisTemplate;
+
+    public CustomerServiceImpl(RedisTemplate<String, Customer> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     @Autowired
     private CustomerRepository customerRepository;
 
     @Override
+    @Transactional
     public Customer saveCustomer(Customer customer) {
-        Customer newCustomer = customerRepository.save(customer);
-        return newCustomer;
+        customer.setId(UUID.randomUUID().toString());
+        redisTemplate.boundValueOps(customer.getId()).set(customer);
+        redisTemplate.boundSetOps(WRITE_BACK_CACHE_KEY).add(customer);
+        return customer;
     }
 
     @Override
@@ -59,14 +75,21 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     @Override
-    @Cacheable(key = "#id")
     public Customer getCustomerById(String id) {
-        Customer fetchedCustomer = customerRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Resource not found"));
-        return fetchedCustomer;
+        final var customerOnCache = redisTemplate.boundValueOps(id).get();
+        if (customerOnCache != null) {
+            log.info("Customer retrieved from cache (customer Id={})", id);
+            return customerOnCache;
+        }
+        final var customerNotCached = customerRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Resource not found"));
+            log.info("customer retrieved from database (customer Id={})", id);
+            redisTemplate.boundValueOps(customerNotCached.getId()).set(customerNotCached);
+            redisTemplate.boundSetOps(WRITE_BACK_CACHE_KEY).add(customerNotCached);
+            log.info("Person cached (key={}, value={})", id, customerNotCached);
+        return customerNotCached;
     }
 
     @Override
-    @CachePut(key = "#id")
     public Customer updateCustomerById(Customer customer, String id) {
         Customer fetchedCustomer = customerRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Resource not found"));
 
@@ -82,10 +105,37 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     @Override
-    @CacheEvict(key = "#id")
     public void deleteCustomerById(String id) {
         Customer fetchedCustomer = customerRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Resource not found"));
         customerRepository.delete(fetchedCustomer);
+    }
+    @Scheduled(fixedRateString = "${customer-service.cache.write-back-rate}")
+    public void writeBack() {
+        log.info("write back scheduler invoked");
+        final var amountOfPeopleToPersist = redisTemplate.boundSetOps(WRITE_BACK_CACHE_KEY).size();
+        if (amountOfPeopleToPersist == null || amountOfPeopleToPersist == 0) {
+            log.info("None customer to write back from cache to database");
+            return;
+        }
+
+        log.info("Found {} customer to write back from cache to database", amountOfPeopleToPersist);
+        final var setOperations = redisTemplate.boundSetOps(WRITE_BACK_CACHE_KEY);
+        final var scanOptions = ScanOptions.scanOptions().build();
+
+        try (final var cursor = setOperations.scan(scanOptions)) {
+            assert cursor != null;
+            while (cursor.hasNext()) {
+                final var customer = cursor.next();
+                customerRepository.save(customer);
+                log.info("Customer saved (customer={})", customer);
+
+                redisTemplate.boundSetOps(WRITE_BACK_CACHE_KEY).remove(customer);
+                log.info("Customer removed from {} set (customer={})", WRITE_BACK_CACHE_KEY, customer);
+            }
+            log.info("Persisted {} customer in the database", amountOfPeopleToPersist);
+        } catch (RuntimeException exception) {
+            log.error("Error reading {} set from Redis", WRITE_BACK_CACHE_KEY, exception);
+        }
     }
 
 }
